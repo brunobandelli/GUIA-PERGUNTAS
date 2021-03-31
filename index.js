0f231a6e6b1e2c894aa9a9bc9a329a860ecae23f
brunobandelli <brunobandelli@gmail.com>
2021-03-30 22:24:49 -0300

    Update 22:24 30/03/21

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ app.get("/perguntar",(req, res) => {
 });
 
 app.post("/salvarpergunta",(req, res) => {          // ROTA PARA RECEBER OS DADOS DO FORMULARIO
-    var titulo = req.body.titulo;                   // RECEBE OS DADOS DO TITULO NA VAR
-    var descricao =  req.body.descricao;            // RECEBER OS DADOS DA DESCRIÇÃO NA VAR
+    var titulo = req.body.titulo;                   // RECEBE OS DADOS DO TITULO NA VAR PELO NAME TITULO NO perguntar.ejs
+    var descricao =  req.body.descricao;            // RECEBER OS DADOS DA DESCRIÇÃO NA VAR PELO NAME DESCRICAO NO perguntar.ejs
     Pergunta.create({                               // CHAMAR O MODEL Pergunta. // EQUIVALENTE AO CÓDIGO SQL: INSERTE INTO perguntas ...Pergunta.                              
-        titulo: titulo,                             // CAMPO TITULO RECEBE OS DADOS DA VARIAVEL TITULO DO FORMULÁRIO: var titulo = req.body.titulo;
-        descricao: descricao                        // CAMPO DESCRICAO RECEBE OS DADOS DA VARIAVEL DESCRICAO DO FORMULÁRIO: var descricao = req.body.descricao;
+        titulo: titulo,                             // CAMPO TITULO RECEBE OS DADOS DA VARIAVEL TITULO DO FORMULÁRIO: var titulo = req.body.titulo; (perguntar.ejs)
+        descricao: descricao                        // CAMPO DESCRICAO RECEBE OS DADOS DA VARIAVEL DESCRICAO DO FORMULÁRIO: var descricao = req.body.descricao; (perguntar.ejs)
     }).then(() =>{                                  // QUANDO A PERGUNTA FOR SALVA COM SUCESSO NO MEU BANCO DE DADOS, ENTÃO FARÁ O COMANDO A BAIXO:
         res.redirect("/");                          // FARÁ COM QUE O USUARIO SEJÁ REDIRECIONADO PARA A PAGINA PRINCIPAL.
     });
@@ -65,5 +65,14 @@ app.get("/pergunta/:id",(req, res) => {             // ROTA DE CADA ID DAS PERGU
     });
 });
 
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;                     //IRÁ RECEBER O CONTEUDO QUE VEM DA TEXTAREA PELO NAME CORPO NO pergunta.ejs [ req = requisição / body = corpo da requisição / nome do campo ]
+    var perguntaId = req.body.pergunta;             //IRÁ RECEBER O ID DINAMICO DE PELO NAME PEGUNTA NO pergunta.ejs
+    Resposta.create({                               //MODEL: Resposta // EQUIVALENTE AO CÓDIGO SQL: INSERTE INTO respotas ...Resposta.
+        corpo: corpo,                               //CAMPO CORPO RECEBE OS DADOS DA VARIAVEL CORPO DO FORMULÁRIO: var corpo = req.body.corpo; (pergunta.ejs)
+        perguntaId: perguntaId                      //CAMPO PERGUNTAID RECEBE OS DADOS DA VARIAVEL PERGUNTAID DO FORMULÁRIO: var perguntaId = req.body.pergunta; (pergunta.ejs)
+
+    });
+})
 
 app.listen(8080,()=>{console.log("App rodando!");});
